4b7fc042692d44e1ca62943ff77f3739d59b7d08
JuanCarlosPerezDelgado <juankapichi_39@hotmail.com>
2025-03-19 21:27:14 +0100

    Update app_prueba.py

--- app_prueba.py
+++ app_prueba.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title("Vista 360 interactiva")
-st.components.v1.iframe("http://localhost:8000/pannellum.html", width=700, height=500)
+st.components.v1.iframe("pannellum.html", width=700, height=500)
 
 
 

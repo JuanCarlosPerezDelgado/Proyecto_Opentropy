677ff6120783edd215fc1e70507c62ddf8809cb7
JuanCarlosPerezDelgado <juankapichi_39@hotmail.com>
2025-04-20 13:55:54 +0200

    Update app_prueba.py

--- app_prueba.py
+++ app_prueba.py
@@ -1,12 +1,8 @@
 import streamlit as st
 
 st.title("Laboratorio Virtual")
-#st.components.v1.iframe("https://juancarlosperezdelgado.github.io/Imagenes/index.html", width=1000, height=1000)
+st.components.v1.iframe("https://juancarlosperezdelgado.github.io/Imagenes/index.html", width=1000, height=1000)
 
 
-st.write(f"""
-        <div style="width: 1000; height: 1000px;">
-            <iframe src="https://juancarlosperezdelgado.github.io/Imagenes/index.html" style="border: none;"></iframe>
-        </div>
-        """,unsafe_allow_html=True)
+
 
